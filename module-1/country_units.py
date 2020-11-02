04f0a9f1dda49fe103599aba7e17daa3f943e4e4
Rostyslav Makhanko <rostyslavm@wix.com>
2020-11-02 18:05:42 +0200

    Add get population functions

--- module-1/country_units.py
+++ module-1/country_units.py
@@ -1,4 +1,6 @@
-class Locality(object):
+import abc
+
+class Locality(abc.ABC):
     def __init__(self, name, locality_type, population):
         self.name = name
         self.locality_type = locality_type
@@ -9,6 +11,15 @@ class Locality(object):
 
     def get_locality_type(self):
         return self.locality_type
+
+    def is_village(self):
+        return self.locality_type == "village"
+
+    def is_town(self):
+        return self.locality_type == "town"
+
+    def is_city(self):
+        return self.locality_type == "city"
 
     def __str__(self):
         return "%s is a %s. Its population is %d" % (self.name, self.locality_type, self.population)
@@ -25,24 +36,53 @@ class City(Locality):
     def __init__(self, name, population):
         Locality.__init__(self, name, "city", population)
 
-class AdministrativeDivision(object):
+class AdministrativeDivision(abc.ABC):
     def __init__(self, name, administrative_division_type, units):
         self.name = name
         self.administrative_division_type = administrative_division_type
         self.units = units
 
+    @abc.abstractmethod
+    def get_population(self):
+        return
+        
+
 class TerritorialCommunity(AdministrativeDivision):
     def __init__(self, name, localities=[]):
         AdministrativeDivision.__init__(self, name, "territorial_community", localities)
+    
+    def get_population(self):
+        population = 0
+        for locality in self.units:
+            population += locality.population
+        return population
 
 class District(AdministrativeDivision):
     def __init__(self, name, localities=[]):
         AdministrativeDivision.__init__(self, name, "district", localities)
 
+    def get_population(self):
+        population = 0
+        for locality in self.units:
+            population += locality.population
+        return population
+
 class Region(AdministrativeDivision):
     def __init__(self, name, districts=[]):
         AdministrativeDivision.__init__(self, name, "region", districts)
 
+    def get_population(self):
+        population = 0
+        for district in self.units:
+            population += district.get_population()
+        return population
+
 class Country(AdministrativeDivision):
     def __init__(self, name, regions=[]):
         AdministrativeDivision.__init__(self, name, "country", regions)
+
+    def get_population(self):
+        population = 0
+        for region in self.units:
+            population += region.get_population()
+        return population
--- module-1/example.py
+++ module-1/example.py
@@ -6,6 +6,26 @@ tomyna_balka = cu.Village("Tomyna Balka", 1675)
 
 chornobayivka = cu.Village("Chornobayivka", 4343)
 
+kherson = cu.Town("Kherson", 220000)
+
 bilozerska_otg = cu.TerritorialCommunity("Bilozerka Territory Community", [nadezhdivka, bilozerka, tomyna_balka])
 
-khersonskyi_district = cu.District("Kherson District", [nadezhdivka, bilozerka, tomyna_balka, chornobayivka])
+khersonskyi_district = cu.District("Kherson District", [nadezhdivka, bilozerka, tomyna_balka, chornobayivka, kherson])
+
+rozdolne = cu.Village("Rozdolne", 2312)
+vilna_ukraina = cu.Village("Vilna Ukarina", 659)
+kakhovka = cu.Town("Kakhovka", 15900)
+
+kakhovska_otg = cu.TerritorialCommunity("Kakhovka Territory Community", [kakhovka, vilna_ukraina, rozdolne])
+
+kakhovskyi_district = cu.District("Kakhovka District", [kakhovka, vilna_ukraina, rozdolne])
+
+kherson_region = cu.Region("kherson Region", [khersonskyi_district, kakhovskyi_district])
+
+print(bilozerka.get_locality_type())
+
+print(nadezhdivka.is_village())
+
+print(bilozerska_otg.get_population())
+
+print(kherson_region.get_population())
